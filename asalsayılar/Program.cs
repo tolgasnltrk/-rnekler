e055bd56d03e62062fe08b2726668dce3483a612
Lenovo <Lenovo@DESKTOP-V17PEFS>
2021-11-09 13:47:45 +0300

    Add project files.

---
+++ asalsayılar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asalsayılar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1.başla
+             * 2.kullanıcıdan pozitif bir tam sayı alınır.
+             * 3.kullanıcıdan alınan sayı 2'den başlayarak kullanıcının girdiği sayıya kadar bölünür ve kalanı bulunur
+             * 4.eğer bölüm işleminden kalan 0 ise asal sayı değildir
+             * 5.eğer bölüm işleminden kalan değilse asal sayıdır
+             * 6.kullanıcıya asal sayı durumu gösterilir
+             * 7.bitiş
+             */
+            Console.Write("pozitif tam sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < sayi; sayi++)
+                
+        }
+       
+    }
+}
